05a92d8fe9acf155577f7e688d1e622b2090a8b4
NickDub <dubois.nicolas@free.fr>
2019-09-25 20:10:46 +0200

    feat: add plug and surge 3+1

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ module.exports = {
    *  - name: device usual name (optional, if not set, default device name will be used)
    */
   devices: [
-    { id: '63701040xxxxxxxxxxxx', key: 'xxxxxxxxxxxxxxxx', type: 'switch_1', name: 'Lampe salon' }, // Simple On/Off switch
-    { id: '07607580xxxxxxxxxxxx', key: 'xxxxxxxxxxxxxxxx', type: 'curtain', name: 'Volet Chambre' }, // Volet roulant
+    { id: '63701040xxxxxxxxxxxx', key: 'xxxxxxxxxxxxxxxx', version: 3.1, type: 'switch_1', name: 'Lampe salon' }, // Simple On/Off switch
+    { id: '07607580xxxxxxxxxxxx', key: 'xxxxxxxxxxxxxxxx', version: 3.1, type: 'curtain', name: 'Volet Chambre' }, // Volet roulant
+    { id: '85502354xxxxxxxxxxxx', key: 'xxxxxxxxxxxxxxxx', version: 3.1, type: 'surge_31', name: 'Multi-prise Salon' }, // Multi-prise
   ]
 };
